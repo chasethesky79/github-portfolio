efcd727550bd599ba9cb2bbd8849fd27081f6efc
Bharath Seshadri <bharathseshadri@Bharaths-MacBook-Pro.local>
2021-02-04 23:18:56 -0600

    portfolio-branch
    Added the styled components to a common file

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,17 +1,5 @@
-import styled from 'styled-components';
+import { ListWrapper, ListItem, Label } from '../styled-components/styled-components'
 
-const ListWrapper = styled.ul`
-list-style: none;
-text-align: left;
-padding: 0px;
-`
-const ListItem = styled.li`
-display: flex;
-justify-content: space-between;
-`
-const Label = styled.span`
- font-weight: bold;
-`
 export default function List({ items }) {
     return (
         <ListWrapper>
---
+++ src/styled-components/styled-components.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const ListWrapper = styled.ul`
+list-style: none;
+text-align: left;
+padding: 0px;
+`
+export const ListItem = styled.li`
+display: flex;
+justify-content: space-between;
+`
+export const Label = styled.span`
+ font-weight: bold;
+`
